ddb4e16ad76e3c51b6fdb17dc5ee0ca7dbee8aa5
Alexandre <alexandre@Alexandres-MacBook-Pro.local>
2019-12-15 12:40:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # svg_font_builder
 Simple gulp to convert SVGs into Fonts
+
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd svg_font_builder
+$ npm install
+$ gulp myfont
+```
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    consolidate = require('gulp-consolidate'),
+    iconfont = require('gulp-iconfont');
+
+gulp.task('myfont', function () {
+    return gulp.src('src/svg/*.svg')
+        .pipe(iconfont({
+            fontName: 'myfont',
+            formats: ['ttf', 'eot', 'woff', 'woff2'],
+            appendCodepoints: true,
+            appendUnicode: false,
+            normalize: true,
+            fontHeight: 1000,
+            centerHorizontally: true
+        }))
+        .on('glyphs', function (glyphs, options) {
+            gulp.src('src/myfont.css')
+                .pipe(consolidate('underscore', {
+                    glyphs: glyphs,
+                    fontName: options.fontName,
+                    fontDate: new Date().getTime()
+                }))
+                .pipe(gulp.dest('myfont'));
+
+            gulp.src('src/index.html')
+                .pipe(consolidate('underscore', {
+                    glyphs: glyphs,
+                    fontName: options.fontName
+                }))
+                .pipe(gulp.dest('myfont'));
+        })
+        .pipe(gulp.dest('myfont'));
+});
